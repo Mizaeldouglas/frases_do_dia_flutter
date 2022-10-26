b15dfece10a6f5ca6c90f143eb2c0ea27dba7bd7
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-10-26 10:47:02 -0300

    Projeto concluído

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,92 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(MyApp());
+void main(){
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Frases do dia",
+    home: Home(),
+  ));
+
 }
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Home extends StatefulWidget {
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
 
-  void _incrementCounter() {
+
+  var _frases = [
+    "Sou apenas um pequeno planeta que se perde diariamente em todo o seu universo.",
+    "Novas amizades serão sempre bem-vindas para darem cor e alegria ao meu dia a dia.",
+    "Gratidão não é pagamento, mas um reconhecimento que se demonstra no dia a dia.",
+    "Nem toda mudança importante acontece de repente e faz barulho, algumas são silenciosas e vão se fazendo no dia a dia.",
+    "Não pago com a mesma moeda, porque gosto de valorizar o troco com nobreza e caráter",
+    "Você só vence amanhã se não desistir hoje",
+    "Não me conte o que eles falaram ao meu respeito, diga-me apenas porque eles estavam tão confortáveis em dizer isso na sua frente.",
+    "Assim como a lua, a vida é resumida em fases",
+    "Seja uma pessoa boa, mas não perca seu tempo provando isso ❤️",
+    "O que ninguém sabe, ninguém estraga✌️",
+    "Enquanto você estiver preso à opniões alheias, não será livre e muito menos feliz. Liberte-se",
+    "O prego que se destaca, é o primeiro a ser martelado",
+    "Raiva é aquele negócio que faz a boca ser mais rápida do que a cabeça.",
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase!";
+
+  void _gerarFrases(){
+    var numerosSoteados = Random().nextInt(_frases.length);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _fraseGerada = _frases[ numerosSoteados ];
     });
   }
 
+
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text( "Frase do dia" ),
+        backgroundColor: Colors.black,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("images/logo.png"),
+              Text(
+                _fraseGerada,
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.black,
+                  fontStyle: FontStyle.italic
+                ),
+              ),
+              RaisedButton(
+                child: Text(
+                  "Nova Frase",
+                  style: TextStyle(
+                    fontSize: 25,
+                      color: Colors.white,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                  color: Colors.green,
+                  onPressed: _gerarFrases
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
